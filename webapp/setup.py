4d331f3ebc82aacd4f143ee48211e2e173765fcd
lostmartian <sahilgangurde08@gmail.com>
2022-05-03 01:47:27 +0530

    Image upload added

--- webapp/setup.py
+++ webapp/setup.py
@@ -1,45 +1,58 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for, send_from_directory
+from werkzeug.utils import secure_filename
 import os
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import load_img , img_to_array
 
 app = Flask(__name__)
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-model = load_model(os.path.join(BASE_DIR , 'model/efficientnetv2s.h5'))
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
-ALLOWED_EXT = set(['jpg' , 'jpeg' , 'png' , 'jfif'])
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1] in ALLOWED_EXT
+UPLOAD_FOLDER = os.path.join(BASE_DIR, 'bucket')
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'JPG'}
 
-classes = ['Apple___Apple_scab', 'Apple___Black_rot', 'Apple___Cedar_apple_rust', 'Apple___healthy', 'Blueberry___healthy', 'Cherry_(including_sour)___Powdery_mildew', 'Cherry_(including_sour)___healthy', 'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot', 'Corn_(maize)___Common_rust_', 'Corn_(maize)___Northern_Leaf_Blight', 'Corn_(maize)___healthy', 'Grape___Black_rot', 'Grape___Esca_(Black_Measles)', 'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)', 'Grape___healthy', 'Orange___Haunglongbing_(Citrus_greening)', 'Peach___Bacterial_spot', 'Peach___healthy', 'Pepper,_bell___Bacterial_spot', 'Pepper,_bell___healthy', 'Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy', 'Raspberry___healthy', 'Soybean___healthy', 'Squash___Powdery_mildew', 'Strawberry___Leaf_scorch', 'Strawberry___healthy', 'Tomato___Bacterial_spot', 'Tomato___Early_blight', 'Tomato___Late_blight', 'Tomato___Leaf_Mold', 'Tomato___Septoria_leaf_spot', 'Tomato___Spider_mites Two-spotted_spider_mite', 'Tomato___Target_Spot', 'Tomato___Tomato_Yellow_Leaf_Curl_Virus', 'Tomato___Tomato_mosaic_virus', 'Tomato___healthy']
+MODEL = load_model(os.path.join(os.path.join(BASE_DIR, 'model'), 'efficientnetv2s.h5'))
 
-def predict(filename , model):
-    img = load_img(filename , target_size = (224 , 224))
-    img = img_to_array(img)
-    img = img.reshape(1 , 224 ,224 ,3)
-    img = img.astype('float32')
-    img = img/255.0
-    result = model.predict(img)
-    dict_result = {}
-    for i in range(10):
-        dict_result[result[0][i]] = classes[i]
-    res = result[0]
-    res.sort()
-    res = res[::-1]
-    prob = res[:3]
-    
-    prob_result = []
-    class_result = []
-    for i in range(3):
-        prob_result.append((prob[i]*100).round(2))
-        class_result.append(dict_result[prob[i]])
-    return class_result , prob_result
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def home():
         return render_template("index.html")
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/uploads/<name>')
+def download_file(name):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], name)
+
+@app.route('/up', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('download_file', name=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
 
 if __name__== "__main__":
     app.run(debug=True)
