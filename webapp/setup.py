a33432c7caa1db0c458addde4379682d668afda7
lostmartian <sahilgangurde08@gmail.com>
2022-05-04 02:06:43 +0530

    Plant classification done

--- webapp/setup.py
+++ webapp/setup.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, redirect, request, url_for, send_from_directory
+from json import load
+from flask import Flask, render_template, redirect, request, url_for, send_from_directory, flash
 from werkzeug.utils import secure_filename
 import os
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing.image import load_img , img_to_array
+import tensorflow
+import numpy as np
 
 app = Flask(__name__)
 
@@ -11,9 +12,12 @@ BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 UPLOAD_FOLDER = os.path.join(BASE_DIR, 'bucket')
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'JPG'}
 
-MODEL = load_model(os.path.join(os.path.join(BASE_DIR, 'model'), 'efficientnetv2s.h5'))
+MODEL = tensorflow.keras.models.load_model(os.path.join(os.path.join(BASE_DIR, 'model'), 'efficientnetv2s.h5'))
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+CLASSES = ['Apple___Apple_scab', 'Apple___Black_rot', 'Apple___Cedar_apple_rust', 'Apple___healthy', 'Blueberry___healthy', 'Cherry_(including_sour)___Powdery_mildew', 'Cherry_(including_sour)___healthy', 'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot', 'Corn_(maize)___Common_rust_', 'Corn_(maize)___Northern_Leaf_Blight', 'Corn_(maize)___healthy', 'Grape___Black_rot', 'Grape___Esca_(Black_Measles)', 'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)', 'Grape___healthy', 'Orange___Haunglongbing_(Citrus_greening)', 'Peach___Bacterial_spot', 'Peach___healthy', 'Pepper,_bell___Bacterial_spot', 'Pepper,_bell___healthy', 'Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy', 'Raspberry___healthy', 'Soybean___healthy', 'Squash___Powdery_mildew', 'Strawberry___Leaf_scorch', 'Strawberry___healthy', 'Tomato___Bacterial_spot', 'Tomato___Early_blight', 'Tomato___Late_blight', 'Tomato___Leaf_Mold', 'Tomato___Septoria_leaf_spot', 'Tomato___Spider_mites Two-spotted_spider_mite', 'Tomato___Target_Spot', 'Tomato___Tomato_Yellow_Leaf_Curl_Virus', 'Tomato___Tomato_mosaic_virus', 'Tomato___healthy']
+
 
 @app.route('/')
 def home():
@@ -27,10 +31,9 @@ def allowed_file(filename):
 def download_file(name):
     return send_from_directory(app.config["UPLOAD_FOLDER"], name)
 
-@app.route('/up', methods=['GET', 'POST'])
-def upload_file():
+@app.route('/plantdisease', methods=['GET', 'POST'])
+def plantdisease():
     if request.method == 'POST':
-        # check if the post request has the file part
         if 'file' not in request.files:
             flash('No file part')
             return redirect(request.url)
@@ -43,16 +46,19 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            model = MODEL
+            imagefile = tensorflow.keras.utils.load_img(os.path.join(app.config['UPLOAD_FOLDER'], filename), target_size=(224, 224, 3))
+            input_arr = tensorflow.keras.preprocessing.image.img_to_array(imagefile)
+            input_arr = np.array([input_arr])
+            result = model.predict(input_arr)
+            probability_model = tensorflow.keras.Sequential([model, 
+                                         tensorflow.keras.layers.Softmax()])
+            predict = probability_model.predict(input_arr)
+            p = np.argmax(predict[0])
+            res = CLASSES[p]
+            print(res)
             return redirect(url_for('download_file', name=filename))
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''
+    return render_template("plantdisease.html")
 
 if __name__== "__main__":
     app.run(debug=True)
